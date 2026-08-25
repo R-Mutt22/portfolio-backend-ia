9f986a5b1e5dd95d5c94eea21bc4991925ba62c8
Matías Zelarayán <matiaszelarayandev@gmail.com>
2026-08-24 23:49:31 -0300

    Primer commit: Estructura base de Spring Boot 4

---
+++ src/main/java/tech/matiaszelarayan/PortfolioBackendIaApplication.java
@@ -0,0 +1,13 @@
+package tech.matiaszelarayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/matiaszelarayan/PortfolioBackendIaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.matiaszelarayan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
